40ca0c7d888f36e45e63caa326d85c626775bad7
William Liu <liutk.william@gmail.com>
2026-09-12 11:09:59 +1000

    add sideprojects

--- src/App.jsx
+++ src/App.jsx
@@ -12,40 +12,42 @@ import AdminLayout from './layouts/AdminLayout';
 import AdminDashboard from './pages/Admin/AdminDashboard';
 import AdminLogin from './pages/Admin/AdminLogin';
 import RequireAdminAuth from './components/RequireAdminAuth';
+import SideProjects from './pages/SideProjects';
 
-  function App() {
-    return (
-      <Router basename="/">
-        <Routes>
-          {/* default layout */}
-          <Route path="/" element={<Default />}>
-            <Route index element={<Home />} />
-            <Route path="/about" element={<About />} />
+function App() {
+  return (
+    <Router basename="/">
+      <Routes>
+        {/* default layout */}
+        <Route path="/" element={<Default />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Route>
+        {/* page layout */}
+        <Route path="/" element={<Page />}>
+          {/* <Route path="/blog" element={<PortfolioPage type="posts" />} /> */}
+          <Route path="/portfolio" element={<PortfolioPage type="portfolio" />} />
+          <Route path="/developer" element={<PortfolioPage type="developer" />} />
+          {/* <Route path="/gallery" element={<Navigate to="/photography" replace />} /> */}
+          <Route path="sideprojects" element={<SideProjects />} />
+        </Route>
+        {/* portfolio layout */}
+        <Route element={<Portfolio />}>
+          {/* <Route path="/blog/:id" element={<PortfolioEntry type="posts" />} /> */}
+          <Route path="/portfolio/:id" element={<PortfolioEntry type="portfolio" />} />
+          <Route path="/developer/:id" element={<PortfolioEntry type="developer" />} />
+        </Route>
+        {/* Admin */}
+        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route element={<RequireAdminAuth />}>
+          <Route element={<AdminLayout />}>
+            <Route path="/admin" element={<AdminDashboard />} />
           </Route>
-          {/* page layout */}
-          <Route path="/" element={<Page />}>
-            <Route path="/blog" element={<PortfolioPage type="posts" />} />
-            <Route path="/portfolio" element={<PortfolioPage type="portfolio" />} />
-            <Route path="/developer" element={<PortfolioPage type="developer" />} />
-            <Route path="/gallery" element={<Navigate to="/photography" replace />} />
-          </Route>
-          {/* portfolio layout */}
-          <Route element={<Portfolio />}>
-            <Route path="/blog/:id" element={<PortfolioEntry type="posts" />} />
-            <Route path="/portfolio/:id" element={<PortfolioEntry type="portfolio" />} />
-            <Route path="/developer/:id" element={<PortfolioEntry type="developer" />} />
-          </Route>
-          {/* Admin */}
-          <Route path="/admin/login" element={<AdminLogin />} />
-          <Route element={<RequireAdminAuth />}>
-            <Route element={<AdminLayout />}>
-              <Route path="/admin" element={<AdminDashboard />} />
-            </Route>
-          </Route>
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Router>
-    )
-  }
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
+  )
+}
 
 export default App
